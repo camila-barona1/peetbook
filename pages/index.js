a7617d17d9eea1c0f95f1586f4f7b0994176c48e
Maria Camila <68437682+camila-barona1@users.noreply.github.com>
2021-05-24 23:07:19 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,26 @@ export default function Home({ session, posts }) {
   return (
     <div className="h-screen bg-gray-100 overflow-hidden">
       <Head>
+    <meta httpEquiv="etag" content="2efdc27c8967f14e2c829e601f7a1228" />
+        <meta property="og:title" content="South Jersey Aerial Photograpahy" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://www.sjap.online/" />
+        <meta
+          property="og:image"
+          content="https://static.wixstatic.com/media/56a444_9273e80a60684dc8b38e56025059f356%7Emv2_d_3200_1800_s_2.png"
+        />
+        <meta
+          property="og:site_name"
+          content="South Jersey Aerial Photograpahy"
+        />
+        <meta
+          property="og:description"
+          content="South Jersey Aerial Photography is South Jersey's premier aerial photography and aerial videography company. Fully licensed and insured. Contact us today!"
+        />
+        <meta
+          className="SKYPE_TOOLBAR"
+          content="SKYPE_TOOLBAR_PARSER_COMPATIBLE"
+        />
         <title>Petbook</title>
       </Head>
       {/* Header */}
